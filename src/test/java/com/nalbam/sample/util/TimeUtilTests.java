c31f4f31394cb64b964679182783342341b155b5
nalbam <me@nalbam.com>
2018-05-15 13:36:39 +0900

    test-JANUARY

--- src/test/java/com/nalbam/sample/util/TimeUtilTests.java
+++ src/test/java/com/nalbam/sample/util/TimeUtilTests.java
@@ -13,14 +13,14 @@ import static org.junit.Assert.assertNotNull;
 @Slf4j
 public class TimeUtilTests {
 
+    private final SimpleDateFormat f = new SimpleDateFormat("yyyy년 MM월 dd일");
+
     @Test
     public void testDate() {
         final Date d = new Date();
         d.setTime(d.getTime() - 100000);
 
         final String ago = TimeUtil.ago(d);
-
-        final SimpleDateFormat f = new SimpleDateFormat("yyyy년 MM월 dd일");
 
         log.info("# ago : {} 은 {} 입니다.", f.format(d.getTime()), ago);
 
@@ -31,11 +31,9 @@ public class TimeUtilTests {
     @Test
     public void testCalendar() {
         final Calendar c = Calendar.getInstance();
-        c.set(c.get(Calendar.YEAR) - 1, Calendar.APRIL, 28);
+        c.set(c.get(Calendar.YEAR) - 1, Calendar.JANUARY, 1);
 
         final String ago = TimeUtil.ago(c);
-
-        final SimpleDateFormat f = new SimpleDateFormat("yyyy년 MM월 dd일");
 
         log.info("# ago : {} 은 {} 입니다.", f.format(c.getTime()), ago);
 
