00a34d171e6b2b82cebe1cc14c5f56178e920e38
nalbam <me@nalbam.com>
2020-01-02 15:17:54 +0900

    Scheduled on

--- src/main/java/com/nalbam/sample/task/SendTask.java
+++ src/main/java/com/nalbam/sample/task/SendTask.java
@@ -41,7 +41,7 @@ public class SendTask {
         log.info("pulse");
     }
 
-    // @Scheduled(fixedRate = 890)
+    @Scheduled(fixedRate = 890)
     public void dealy() {
         if ("test".equals(profile)) {
             return;
@@ -56,7 +56,7 @@ public class SendTask {
         }
     }
 
-    // @Scheduled(fixedRate = 789)
+    @Scheduled(fixedRate = 789)
     public void fault() {
         if ("test".equals(profile)) {
             return;
@@ -71,7 +71,7 @@ public class SendTask {
         }
     }
 
-    // @Scheduled(fixedRate = 456)
+    @Scheduled(fixedRate = 456)
     public void node() {
         if ("default".equals(profile) || "test".equals(profile)) {
             return;
@@ -84,7 +84,7 @@ public class SendTask {
         call("http://sample-node" + commands.get(random.nextInt(commands.size())));
     }
 
-    // @Scheduled(fixedRate = 567)
+    @Scheduled(fixedRate = 567)
     public void stress() {
         if ("default".equals(profile) || "test".equals(profile)) {
             return;
@@ -97,7 +97,7 @@ public class SendTask {
         call("http://" + commands.get(random.nextInt(commands.size())) + "/stress");
     }
 
-    // @Scheduled(fixedRate = 678)
+    @Scheduled(fixedRate = 678)
     public void spring() {
         if ("default".equals(profile) || "test".equals(profile)) {
             return;
