833fe8682a2965dad4daed54123276eff5399b54
nalbam <me@nalbam.com>
2020-01-03 15:20:57 +0900

    docker run

--- build.sh
+++ build.sh
@@ -8,25 +8,33 @@ fi
 
 echo "VERSION=${VERSION}"
 
-echo "$ mvn package"
+if [ "${1}" == "docker" ] || [ "${1}" == "run" ]; then
+    echo "$ mvn clean"
+    mvn clean
+fi
+
+echo "$ mvn package -Dthis.version=${VERSION}"
 mvn package -Dthis.version=${VERSION}
 
 if [ "${1}" == "docker" ] || [ "${1}" == "run" ]; then
-    docker build -t nalbam/sample-spring .
+    echo "$ docker build -t nalbam/sample-spring:local ."
+    docker build -t nalbam/sample-spring:local .
 fi
 
-if [ "${1}" == "run" ]; then
+if [ "${1}" == "stop" ] || [ "${1}" == "run" ]; then
     echo "$ docker ps -a"
     docker ps -a
 
     CNT="$(docker ps -a | grep 'nalbam/sample-spring' | wc -l | xargs)"
-    if [ "${CNT}" != "x0" ]; then
+    if [ "x${CNT}" != "x0" ]; then
         docker stop sample-spring
         docker rm sample-spring
     fi
+fi
 
-    echo "$ docker run --name sample-spring -p 8080:8080 -d nalbam/sample-spring"
-    docker run --name sample-spring -p 8080:8080 -d nalbam/sample-spring
+if [ "${1}" == "run" ]; then
+    echo "$ docker run --name sample-spring -p 8080:8080 -d nalbam/sample-spring:local"
+    docker run --name sample-spring -p 8080:8080 -d nalbam/sample-spring:local
 
     echo "$ docker ps -a"
     docker ps -a
