7d24239ae7e1d1731a33bb7459d5aa4658b18b3f
nalbam <me@nalbam.com>
2020-01-08 14:53:38 +0900

    JAVA_OPTS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
-
 if [ "${JMX_ENABLED}" == "true" ]; then
     JMX_PATH=${JMX_PATH:-"/data/jmx_javaagent.jar"}
     JMX_PORT=${JMX_PORT:-8081}
