2cd2e667022e209b077c2db7d25e85944f2b9cf4
nalbam <me@nalbam.com>
2020-05-24 21:46:19 +0900

    JAVA_AGENT

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if [ "${DD_AGENT_ENABLED}" == "true" ]; then
-    export JAVA_AGENT_PATH=${JAVA_AGENT_PATH:-"/data/dd-java-agent.jar"}
+    export JAVA_AGENT_PATH="/data/dd-java-agent.jar"
     export JAVA_OPTS="${JAVA_OPTS} -javaagent:${JAVA_AGENT_PATH}"
 fi
 
 if [ "${JAVA_AGENT_ENABLED}" == "true" ]; then
-    export JAVA_AGENT_CONFIG=${JAVA_AGENT_CONFIG:-"/data/config.yaml"}
-    export JAVA_AGENT_PATH=${JAVA_AGENT_PATH:-"/data/jmx_javaagent.jar"}
-    export JAVA_AGENT_PORT=${JAVA_AGENT_PORT:-8081}
+    export JAVA_AGENT_CONFIG="/data/config.yaml"
+    export JAVA_AGENT_PATH="/data/jmx_javaagent.jar"
+    export JAVA_AGENT_PORT="8081"
     export JAVA_OPTS="${JAVA_OPTS} -javaagent:${JAVA_AGENT_PATH}=${JAVA_AGENT_PORT}:${JAVA_AGENT_CONFIG}"
 fi
 
