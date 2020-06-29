6bc6a6898fa5d9e36d8954bfd91e3ddf049fabfa
nalbam <me@nalbam.com>
2020-06-29 17:27:02 +0900

    cp jmx/jmx_*

--- build.sh
+++ build.sh
@@ -109,7 +109,7 @@ mvn_build() {
     if [ -f jmx/config.yaml ]; then
         mkdir -p target/jmx
         cp jmx/config.yaml target/jmx/config.yaml
-        cp jmx/jmx_javaagent.jar.zip target/jmx/jmx_javaagent.jar
+        cp jmx/jmx_* target/jmx/jmx_javaagent.jar
     fi
 }
 
