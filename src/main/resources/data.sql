4d8d06bac4cd0c43379f756e3acac5daccd1e735
nalbam <me@nalbam.com>
2021-01-05 19:27:35 +0900

    rm jdbc

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO customer(name,email,created_date)VALUES('me','me@nalbam.com', '2020-02-11');
-INSERT INTO customer(name,email,created_date)VALUES('my','my@nalbam.com', '2020-02-12');
-INSERT INTO customer(name,email,created_date)VALUES('bot','bot@nalbam.com', '2020-02-13');
